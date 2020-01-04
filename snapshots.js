51e16e947627c7773dabc346331f2a4705ae9877
serhatgunes <serhat.gunes.92@gmail.com>
2020-01-04 19:50:27 +0300

    snapshot changed

--- snapshots.js
+++ snapshots.js
@@ -3,7 +3,7 @@ const PercyScript = require('@percy/script');
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8000');
+  await page.goto('http://localhost:10001');
   await percySnapshot('TodoMVC home page');
 
   // Enter a new to-do.
